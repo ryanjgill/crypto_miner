bfad148cae6cd8cf55ca89d9838e0dcd7dce0ccd
Ryan Gill <ryanjgill2@gmail.com>
2018-01-01 18:04:21 -0600

    crypto miner

---
+++ public/js/index.js
@@ -0,0 +1,181 @@
+function getRandomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let dummyData = (min, max, count) => {
+	let data = []
+	let _count = count ? count : 10
+	
+	for(let x = 0; x < _count; x++) {
+		data.push([new Date().getTime() - ((_count - x)* 2000), getRandomNumber(min, max)])
+	}
+	
+	return data;
+}
+
+Vue.use(VueHighcharts);
+
+var options = {
+  colors: ["#7cb5ec", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"],
+	credits: false,
+	chart: {
+		style: {
+			fontFamily: "Roboto",
+      fontSize: '0.8em'
+		},
+		backgroundColor: '#272b30'
+	},
+  title: {
+    text: ''
+  },
+  xAxis: {
+		type: 'datetime',
+		tickPixelInterval: 5000
+	},
+  yAxis: {
+    allowDecimals: false,
+		labels: {
+			style: {
+        fontSize: '1.2em',
+        color: '#aaaaaa'
+			}
+		},
+    title: {
+      text: 'Temperature (°C)'
+    },
+    plotLines: [{
+      value: 0,
+      width: 1,
+      color: '#808080'
+    }]
+  },
+  tooltip: {
+    valueSuffix: '°C',
+		enabled: false
+  },
+  legend: {
+    layout: 'horizontal',
+    align: 'center',
+    verticalAlign: 'top',
+    borderWidth: 0,
+		labelFormatter() {
+			return this.name.toUpperCase().split('_').join(' ').split('TEMP').join(' ')
+    },
+    itemStyle: {
+      color: '#aaaaaa'
+    }
+  },
+	plotOptions: {
+    series: {
+        enableMouseTracking: false
+    }
+	},
+  series: [{
+    name: 'room_temp',
+    data: dummyData(31, 33)
+  }, {
+    name: 'case_temp',
+    data: dummyData(45, 49)
+  }, {
+    name: 'radiator_temp',
+    data: dummyData(49, 54)
+  }, {
+    name: 'gpu_1_temp',
+    data: dummyData(65, 68)
+  }, {
+    name: 'gpu_2_temp',
+    data: dummyData(65, 69)
+  }]
+};
+
+new Vue({
+	el: '#dashboard',
+	template: `
+	<div>
+		<div class="rig-name-and-rate readings">
+			<div class="reading">
+				<div>
+					<label>Rig Name</label>
+					<span>{{name}}</span>
+				</div>
+			</div>
+			<div class="reading">
+				<div>
+					<label>Hash Rate</label>
+					<span title="Megahashes per second">{{hashRate}}</span>
+				</div>
+			</div>
+		</div>
+		<div class="readings">
+			<div class="reading">
+				<div>
+					<label>Room</label>
+					<span>{{room_temp}}</span>
+				</div>
+			</div>
+			<div class="reading">
+				<div>
+					<label>Case</label>
+					<span>{{case_temp}}</span>
+				</div>
+			</div>
+			<div class="reading">
+				<div>
+					<label>Radiator</label>
+					<span>{{radiator_temp}}</span>
+				</div>
+			</div>
+			<div class="reading">
+				<div>
+					<label>GPU 1</label>
+					<span>{{gpu_1_temp}}</span>
+				</div>
+			</div>
+			<div class="reading">
+				<div>
+					<label>GPU 2</label>
+					<span>{{gpu_2_temp}}</span>
+				</div>
+			</div>
+		</div>
+		<div id="chart">
+			<highcharts :options="options" ref="highcharts"></highcharts>
+		</div>
+	</div>
+	`,
+	data: {
+		options: options,
+		case_temp: '0',
+		name: 'Gill',
+		gpu_1_temp: '0',
+    gpu_2_temp: '0',
+    socket: null,
+		radiator_temp: '0',
+		room_temp: '0',
+		hashRate: 0
+	},
+	computed: {
+	},
+	created() {
+    this.socket = io();
+
+    this.socket.on('reading', this.handleReading)
+  },
+  methods: {
+    handleReading(data) {
+      Object.keys(data).forEach(key => this.$data[key] = (+data[key]).toFixed(0))
+      this.updateChart()
+    },
+    updateChart() {
+      let series = this.$refs.highcharts.chart.series
+      let ticks = new Date().getTime()
+			series[0].addPoint([ticks, +this.$data.room_temp], false, true)
+			series[1].addPoint([ticks, +this.$data.case_temp], false, true)
+			series[2].addPoint([ticks, +this.$data.radiator_temp], false, true)
+			series[3].addPoint([ticks, +this.$data.gpu_1_temp], false, true)
+			series[4].addPoint([ticks, +this.$data.gpu_2_temp], true, true)
+    }
+  }
+});
---
+++ server.js
@@ -0,0 +1,123 @@
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path')
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const ip = require('ip')
+const Request = require('request-promise')
+const config = require('./config')
+const PORT = 3030
+const SAVE_DELAY = 1000 * 10 // every 10 seconds
+const serverIP = `${ip.address()}:${PORT}`
+const r = require('rethinkdbdash')({
+  db: 'crypto_miner_monitor'
+})
+
+let lastSaveTime = 0
+let hashRates = {
+  gill: 0,
+  mdt: 0
+}
+let lastReading = {}
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(morgan('tiny'))
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+  next()
+})
+
+app.get('/lastReading', (req, res, next) => {
+  res.json(lastReading)
+  next()
+})
+
+io.on('connection', client => {
+  console.log('New client connected.')
+  console.log('Total clients: ', io.engine.clientsCount)
+
+  client.on('resetRig', data => {
+    console.log('resetRig: ', data)
+  });
+
+  client.on('disconnect', () => {
+    console.log('client left.')
+  });
+});
+
+// POST temperatures
+app.post('/temperatures', (req, res, next) => {
+  let body = JSON.parse(JSON.stringify(req.body))
+  let reading = {
+    case_temp: body.case_temp,
+    date: new Date(),
+    gpu_1_temp: body.gpu_1_temp,
+    gpu_2_temp: body.gpu_2_temp,
+    radiator_temp: body.radiator_temp,
+    rig_name: body.rig_name,
+    room_temp: body.room_temp,
+    hashRate: hashRates.gill,
+    gill_hash_rate: hashRates.gill,
+    mdt_hash_rate: hashRates.mdt
+  }
+
+  lastReading = reading
+
+  io.sockets.emit('reading', reading)
+
+  if (new Date().getTime() - lastSaveTime > SAVE_DELAY) {
+    r.table('temperatures').insert(reading).run()
+      .then(function(result) {
+        lastSaveTime = new Date().getTime()
+        res.sendStatus(200)
+        console.log('Reading saved.')
+        next()
+      })
+      .catch(err => {
+        console.log(err)
+        res.sendStatus(500)
+        next()
+      });
+  } else {
+    res.sendStatus(200)
+    next()
+  }
+})
+
+let updateHashRate = (address, name) => {
+  let options = {
+    uri: `https://api.ethermine.org/miner/${address}/currentStats`,
+    json: true
+  };
+
+  Request(options)
+    .then(res => {
+      // convert hash rate to Megahashes/second (Mh/s)
+      hashRate = res.data.currentHashrate
+        ? ((res.data.currentHashrate / 1000000).toFixed(1))
+        : 0
+
+      hashRates[name] = hashRate
+      console.log(`${name} hashRate: ${hashRate} Mh/s`)
+    })
+    .catch(err => console.log(err))
+}
+
+// update hashrates every minute
+setInterval(() => {
+  updateHashRate(config.minerAddresses.gill, 'gill')
+  updateHashRate(config.minerAddresses.mdt, 'mdt')
+}, 1000 * 60)
+
+updateHashRate(config.minerAddresses.gill, 'gill')
+updateHashRate(config.minerAddresses.mdt, 'mdt')
+
+server.listen(PORT, () => console.log(`API listening on ${serverIP}`))
